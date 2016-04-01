d90aec1da2681640a7cdfb2eeb5534de41f76a9c
vinicius <vinicius@vinicius-System-Product-Name>
2016-04-01 07:36:14 -0300

    usando o json eficientemente

--- src/main/java/org/SpringMvc/loja/controller/ProdutosController.java
+++ src/main/java/org/SpringMvc/loja/controller/ProdutosController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -86,6 +87,14 @@ public class ProdutosController {
 		return modelAndView;
 	}
 
+	@RequestMapping("/{id}")
+ 	@ResponseBody
+	public Produto detalheJson(@PathVariable("id") Integer id) {
+				
+		return produtoDao.find(id);
+
+	}
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView listar() {
 		List<Produto> produtos = produtoDao.listar();

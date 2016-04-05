57fe08cfe78bcea67bc193e5e39911437a728e48
vinicius <vinicius@vinicius-System-Product-Name>
2016-04-04 23:57:51 -0300

    SecurityConfiguration Corrigido

--- src/main/java/org/SpringMvc/loja/conf/SecurityConfiguration.java
+++ src/main/java/org/SpringMvc/loja/conf/SecurityConfiguration.java
@@ -1,23 +1,26 @@
 package org.SpringMvc.loja.conf;
 
-
-import org.springframework.core.convert.support.ConfigurableConversionService;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
 
-@EnableWebSecurity
-public class SecurityConfiguration  extends WebMvcConfigurerAdapter{
-	
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
 	@Override
-	CON
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/produtos/form").hasRole("ADMIN")
+        .antMatchers("carrinho").permitAll()
+        .antMatchers("/produtos").hasRole("ADMIN")
+        .antMatchers("/produtos/**").permitAll()
+        .antMatchers("/").permitAll()
+        .anyRequest().authenticated().and().formLogin();
 
-
-
+    }
 
 
 }
-
-
